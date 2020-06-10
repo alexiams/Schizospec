24626f29e02d15ec029dd04bed0f1bd905b0e120
alexiams <59436471+alexiams@users.noreply.github.com>
2020-06-09 22:01:55 -0700

    formatting ig

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 /**Please please go support the amazing author over at 'https://ihatetomatoes.net/create-custom-preloading-screen/'
 Sir, I owe you my life.**/
 
-$(document).ready(function() {
-
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 5000);
-
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").addClass("loaded");
+    $("h1").css("color", "#222222");
+  }, 5000);
 });
